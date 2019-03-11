63115c81fdd524752fdbeb08ec8f363f69858970
Kevin <kevin@Kevins-MBP.fios-router.home>
2019-03-11 00:57:39 -0400

    loading indicator while saving post

--- components/CreatePost/CreatePostContainer.js
+++ components/CreatePost/CreatePostContainer.js
@@ -3,7 +3,10 @@ import {NavigationActions, StackActions} from 'react-navigation';
 import {SafeAreaView, StyleSheet} from 'react-native';
 
 import {CreatePost} from './CreatePost';
+import Loading from '../common/Loading';
+
 import {locationService, postService} from '../../services';
+
 import defaultStyles from '../../styles';
 import { defaultNavigationOptions } from '../../constants';
 
@@ -13,7 +16,8 @@ export class CreatePostContainer extends Component {
 
         this.state = {
             title: '',
-            body: ''
+            body: '',
+            loading: false
         }
     }
 
@@ -22,31 +26,42 @@ export class CreatePostContainer extends Component {
     })
 
     savePost = async () => {
-        const locationData = await locationService.getLocation();
+        this.setState({loading: true});
 
-        const post = await postService.createPost({...this.state}, locationData.coords);
-        
-        const resetAction = StackActions.reset({
-            index: 0, 
-            actions: [NavigationActions.navigate({ routeName: 'Home' })]
-        })
-        
-        this.props.navigation.dispatch(resetAction);
-        this.props.navigation.navigate('Post', {post});
+        try {
+            const locationData = await locationService.getLocation();
+
+            const post = await postService.createPost({...this.state}, locationData.coords);
+            
+            const resetAction = StackActions.reset({
+                index: 0, 
+                actions: [NavigationActions.navigate({ routeName: 'Home' })]
+            })
+            
+            this.props.navigation.dispatch(resetAction);
+            this.props.navigation.navigate('Post', {post});
+        } catch(e) {
+            console.log(e);
+        } finally{
+            this.setState({loading: false});
+        }
     }
 
     render() {
-        const {body, title} = this.state;
+        const {body, title, loading} = this.state;
 
         return (
             <SafeAreaView style={{...defaultStyles.container, ...styles.container}}>
-                <CreatePost 
-                    onPostChange={body => this.setState({body})}
-                    onTitleChange={title => this.setState({title})}
-                    savePost={this.savePost}
-                    post={body}
-                    title={title} 
-                />
+                <Loading loading={loading}/>
+                {!loading && 
+                    <CreatePost 
+                        onPostChange={body => this.setState({body})}
+                        onTitleChange={title => this.setState({title})}
+                        savePost={this.savePost}
+                        post={body}
+                        title={title} 
+                    />
+                }
             </SafeAreaView>
         )
     }
