7aea61645d46568ed9cb6fcdedd967a6bc0d883c
Kevin <kevin@Kevins-MBP.fios-router.home>
2019-03-11 00:55:04 -0400

    add loading when saving comment

--- components/AddComment/AddCommentContainer.js
+++ components/AddComment/AddCommentContainer.js
@@ -2,7 +2,10 @@ import React, {Component} from 'react';
 import {SafeAreaView} from 'react-native';
 
 import {AddComment} from './AddComment';
+import Loading from '../common/Loading';
+
 import { postService } from '../../services';
+
 import defaultStyles from '../../styles';
 import { defaultNavigationOptions } from '../../constants';
 
@@ -11,7 +14,8 @@ export class AddCommentContainer extends Component {
         super(props);
 
         this.state = {
-            comment: ''
+            comment: '',
+            loading: false
         }
     }
 
@@ -20,28 +24,39 @@ export class AddCommentContainer extends Component {
       })
 
     addComment = async () => {
-        const post = this.props.navigation.getParam('post');
+        this.setState({loading: true});
 
-        await postService.addComment(post.id, this.state.comment);
+        try{
+            const post = this.props.navigation.getParam('post');
 
-        this.props.navigation.goBack();
+            await postService.addComment(post.id, this.state.comment);
+
+            this.props.navigation.goBack();
+        } catch(e) {
+            console.log(e);
+        } finally {
+            this.setState({loading: false});
+        }
     }
 
     onCommentChange = comment => this.setState({comment})
 
     render() {
         const post = this.props.navigation.getParam('post');
-        const {comment} = this.state;
+        const {comment, loading} = this.state;
 
         return (
             <SafeAreaView style={defaultStyles.container}>
-                <AddComment 
-                    addComment={this.addComment}
-                    onCommentChange={this.onCommentChange}
-                    postAuthor={post.author_id}
-                    postBody={post.body}
-                    comment={comment}
-                />
+                <Loading loading={loading}/>
+                {!loading && 
+                    <AddComment 
+                        addComment={this.addComment}
+                        onCommentChange={this.onCommentChange}
+                        postAuthor={post.author_id}
+                        postBody={post.body}
+                        comment={comment}
+                    />
+                }
             </SafeAreaView>
         )
     }
